f212f7c5ab58ffb4929c8ab3a76773ff2efe91f5
OldAAAA <1074201527@qq.com>
2020-01-08 19:06:35 +0800

    欢乐的时光开始了

---
+++ include/config.h
@@ -0,0 +1,14 @@
+//
+// Created by ivip on 2020/1/8.
+//
+
+#ifndef MYSLAM_CONFIG_H
+#define MYSLAM_CONFIG_H
+
+
+class config {
+
+};
+
+
+#endif //MYSLAM_CONFIG_H
---
+++ src/config.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivip on 2020/1/8.
+//
+
+#include "../include/config.h"
